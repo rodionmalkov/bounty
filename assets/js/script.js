37347bdd225d3ad13f810ea255920f4511f0bda9
Rodion Malkov <rodionmalkov@gmail.com>
2022-06-26 09:50:35 +0500

    тест свайпера

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-  // $(".carousel").slick();
+var swiper = new Swiper(".carousel", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
